c41c44c8d5afb5986768b879026fc88f8cfab12b
Sara Chen <52257000+saracdata@users.noreply.github.com>
2024-05-04 16:18:25 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from sys import exit
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(60)
