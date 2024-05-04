e6abdf7e0bcac92681a747c7f4dbc49385266171
sarac <sara168818@gmail.com>
2024-05-04 16:47:32 -0700

    changes to main

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font()
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
     pygame.display.update()
     clock.tick(60)
 
