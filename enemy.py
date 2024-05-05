5acc5f779058f7965c02dea934ca067c865de4e0
Anknguyen <akimnguyen1999@gmail.com>
2024-05-04 21:26:53 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- enemy.py
+++ enemy.py
@@ -1,8 +1,25 @@
 import pygame as pg
+from pygame.math import Vector2
 
 class Enemy(pg.sprite.Sprite):
-    def __init__(self, pos, image):
+    def __init__(self, waypoints, image):
         pg.sprite.Sprite.__init__(self)
+        self.waypoints = waypoints
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.center = self.pos
+        self.speed = 2
+
+    def update(self):
+        self.move()
+
+    def move(self):
+        self.target = Vector2(self.waypoints[self.target_waypoint])
+        self.movement = self.target - self.pos
+
+        #calc distance
+
+        self.pos += self.movement.normalize() * self.speed
+        self.rect.center = self.pos
--- main.py
+++ main.py
@@ -10,16 +10,30 @@ clock = pygame.time.Clock()
 #load imgs
 enemy_image = pygame.image.load("graphics/snail1.png").convert_alpha()
 
+waypoints = [
+    (100,100),
+    (400,200),
+    (400,100),
+    (200,300),
+]
+
 #create groups
 enemy_group = pygame.sprite.Group()
 
-enemyName = Enemy((200,300), enemy_image)
+enemyName = Enemy(waypoints, enemy_image)
 enemy_group.add(enemyName)
 
 #game loop
 run = True
 while run:
     clock.tick(const.FPS)
+
+    screen.fill("grey100")
+
+    #draw enemy path
+    pygame.draw.lines(screen, "grey0", False, waypoints)
+    #update groups
+    enemy_group.update()
 
     #draw groups
     enemy_group.draw(screen)
