f1bedaba8749ff133a75d52e0474b283dcccd40c
sarac <sara168818@gmail.com>
2024-11-01 20:47:32 -0700

    added changes to turret class to be able to rotate and update

--- turret.py
+++ turret.py
@@ -1,5 +1,7 @@
+import math
 import pygame as pg
 import constants as c
+
 class Turret(pg.sprite.Sprite):
     def __init__(self, sprite_sheet, tile_x, tile_y, pos):
         pg.sprite.Sprite.__init__(self)
@@ -40,6 +42,10 @@ class Turret(pg.sprite.Sprite):
         self.range_rect = self.range_image.get_rect()
         self.range_rect.center = self.rect.center
 
+        #precompute rotations and add alignment variables
+        self.rotated_images = self.precompute_rotated_images()
+
+
 
 #put other functions as private in other language, each function that start with _
     def load_images(self):
@@ -51,38 +57,58 @@ class Turret(pg.sprite.Sprite):
             animation_list.append(temp_img)
         return animation_list
 
+    def precompute_rotated_images(self):
+        rotated_images = {}
+        for angle in range(0, 360, 15): #pick at chunks of 15 degrees
+            rotated_images[angle] = [pg.transform.rotate(img, angle) for img in self.animation_list]
+        return rotated_images
+
+    def get_rotated_image(self, angle, frame_index):
+        nearest_angle = int(round(angle / 15) * 15) % 360
+        image = self.rotated_images[nearest_angle][frame_index]
+        return image
+
     def update(self, game_started, enemy_group):
-        if game_started:
-            enemy_in_range = False
+        if not game_started:
+            return
 
-            for enemy in enemy_group:
-                if self.in_range(enemy):
-                    enemy_in_range = True
-                    if pg.time.get_ticks() - self.last_shot > self.cooldown:
-                        self.attack(enemy)
-                        self.play_animation()
-                    break # Exit after the first enemy found in range
-            if not enemy_in_range:
-                self.frame_index = 0
-                self.image = self.animation_list[self.frame_index]
-                self.is_animating = False
+        closest_enemy, angle_to_enemy = None, None
+
+        for enemy in enemy_group:
+            if self.in_range(enemy):
+                closest_enemy = enemy
+                dx, dy = enemy.pos.x - self.pos.x, enemy.pos.y - self.pos.y
+                angle_to_enemy = math.degrees(math.atan2(-dy, dx))
+
+                break # Exit after the first enemy found in range
+
+        if closest_enemy:
+            self.target_angle = int(round(angle_to_enemy))
+
+            if pg.time.get_ticks() - self.last_shot > self.cooldown:
+                self.attack(closest_enemy)
+                self.play_animation(angle_to_enemy)
+
+        else:
+            self.frame_index = 0
+            self.image = self.animation_list[self.frame_index]
 
 
-    def play_animation(self):
-        #only animate if we havn't finished the cycle
-        if not self.is_animating:
-            self.is_animating = True
-        #update image
-        self.image = self.animation_list[self.frame_index]
-        #check if enough time has passed since last update
-        if pg.time.get_ticks() - self.update_time > c.ANIMATION_DELAY:
-            self.update_time = pg.time.get_ticks()
+    def play_animation(self, angle_to_enemy):
+
+        self.image = self.get_rotated_image(angle_to_enemy, self.frame_index)
+        if pg.time.get_ticks() - self.update_time <= c.ANIMATION_DELAY:
+            return
+
+        self.update_time = pg.time.get_ticks()
+
+        if self.frame_index < (len(self.animation_list) - 1):
             self.frame_index += 1
-            if self.frame_index >= len(self.animation_list):
-                self.frame_index = 0
-                #record completed time and clear target to start cd
-                self.last_shot = pg.time.get_ticks()
-                self.is_animating = False
+        else:
+            self.frame_index = 0
+            self.last_shot = pg.time.get_ticks()
+
+
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
